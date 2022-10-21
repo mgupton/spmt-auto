04fd009ba74341f846c839ba00908b0f6f905e6e
Michael Gupton <mg@infostar.me>
2022-10-21 13:34:39 -0500

    Making adjustments to figure out how SPMT works

--- spmt-auto.ps1
+++ spmt-auto.ps1
@@ -1,28 +1,9 @@
 #
-# spmt-auto
+# spmt-auto.ps1 is a script that automates the process
+# of running migration jobs with the Sharepoint Migration Tool (SPMT)
 #
 # Created: 2022-10-12
 # Created By: Michael Gupton
-#
-# spmt-auto.ps1 is a script that automates the process
-# of running site migration jobs/tasks with the Sharepoint
-# Migration Tool (SPMT).
-#
-# This script reads the file spmt-auto.json in the current directory
-# that defines the tasks (sites that should be migrated) and the related
-# migration settings.
-# 
-# A custom boolean setting is added to each Task item called RunMigration
-# that is used to indicate whether the specific task should be ran.
-# This is not a setting for SPMT, it's just a setting for the script.
-# It provides the flexibility of being able to add many Tasks to the file,
-# but only running the ones that are needed at a given time.
-#
-# Dependencies:
-#
-# The module Microsoft.SharePoint.MigrationTool.PowerShell that comes
-# with SPMT.
-#
 #
 
 Import-Module Microsoft.SharePoint.MigrationTool.PowerShell
@@ -30,23 +11,24 @@ Import-Module Microsoft.SharePoint.MigrationTool.PowerShell
 $SPCredential = Get-Credential -Message "Please enter Sharepoint Server credentials"
 $SPOCredential = Get-Credential -Message "Please enter Sharepoint Online credentials"
 
-Register-SPMTMigration -SPOCredential $SPOCredential -Force
+# Register the SPMT session with SharePoint credentials
+Register-SPMTMigration -SPOCredential $SPOCredential -Force -ScanOnly $false -AutomaticUserMapping $false
 
 $jsonSettings = Get-Content -Raw -Path  "spmt-auto.json" | ConvertFrom-Json
-foreach ($taskItem in $jsonSettings.Tasks)
+ForEach ($taskItem in $jsonSettings.Tasks)
 {
     if ($taskItem.RunMigration) {
         #
-        # The RunMigration setting is used to indicate the task should be ran.
-        # It's not an actual setting for the SPMT tool,
-        # so it gets removed once it's served its purpose.
-        # It is just a setting for the logic of this script.
+        # The RunMigration is used to indicate the task should be ran.
+        # It's not an actual setting for the SPMT task,
+        # so it gets removed. It is just a setting for
+        # this script.
         #
         $taskItem.PSObject.Properties.Remove("RunMigration")
         $jsonTask = ConvertTo-Json $taskItem -Depth 100
         Add-SPMTTask -JsonDefinition $jsonTask -SharePointSourceCredential $SPCredential
+        Write-Host $jsonTask
     }
 }
 
 Start-SPMTMigration
-Remove-SPMTTask -TaskId (Get-SPMTMigration).Id
